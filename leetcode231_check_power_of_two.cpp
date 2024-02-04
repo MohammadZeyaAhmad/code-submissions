c69d841d85ac4dddb799cba83f9f52823bd52943
Mohammad Zeya Ahmad <mohammadzeyaahmad@gmail.com>
2024-02-04 05:56:49 +0530

    Update and rename check_power_of_two.cpp to leetcode231_check_power_of_two.cpp

--- leetcode231_check_power_of_two.cpp
+++ leetcode231_check_power_of_two.cpp
@@ -5,6 +5,6 @@ class Solution{
     // Function to check if given number n is a power of two.
     bool isPowerofTwo(long long n){
         
-        return n==0?false:(n & (n-1))==0;
+        return n<=0?false:(n & (n-1))==0;
     }
 };
