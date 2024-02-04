b4d9026352a4bfa79a5a11917c145a2553c33d65
Mohammad Zeya Ahmad <mohammadzeyaahmad@gmail.com>
2024-02-04 13:55:56 +0530

    Update leetcode168_ excel_sheet_title.cpp

--- leetcode168_ excel_sheet_title.cpp
+++ leetcode168_ excel_sheet_title.cpp
@@ -1,4 +1,4 @@
-Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+/* Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
 
 For example:
 
@@ -9,4 +9,21 @@ C -> 3
 Z -> 26
 AA -> 27
 AB -> 28 
+ZY ->701
 ...
+*/
+
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+         string result = "";
+    while(columnNumber>0){
+        columnNumber--;
+       int a = columnNumber % 26;
+       result=char(a+'A')+result;
+       columnNumber/=26;
+    }
+    
+    return result;
+    }
+};
