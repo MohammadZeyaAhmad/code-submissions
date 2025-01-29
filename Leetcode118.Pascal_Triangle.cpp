4bea83b134d73d6fefd09f1b99a4d41af91112dd
Mohammad Zeya Ahmad <mohammadzeyaahmad@gmail.com>
2025-01-29 09:33:23 +0530

    Create Leetcode118.Pascal_Triangle.cpp

---
+++ Leetcode118.Pascal_Triangle.cpp
@@ -0,0 +1,23 @@
+/*Given an integer numRows, return the first numRows of Pascal's triangle.*/
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> pascal;
+        pascal.push_back({1});
+        for(int i=0;i<numRows-1;i++){
+            vector<int> temp;
+            for(int j=0;j<pascal[i].size();j++){
+                if(j==0)
+                    temp.push_back(pascal[i][j]);
+                if(j+1<pascal[i].size()){
+                    int sum = pascal[i][j]+pascal[i][j+1];
+                    temp.push_back(sum);
+                }
+                if(j==pascal[i].size()-1)
+                    temp.push_back(pascal[i][j]);
+            }
+            pascal.push_back(temp);
+        }
+        return pascal;
+    }
+};
