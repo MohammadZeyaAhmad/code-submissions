2c49dc97a0198db9cadb31b4b6f6862b15931c0e
Mohammad Zeya Ahmad <mohammadzeyaahmad@gmail.com>
2025-02-04 05:46:17 +0530

    Create print_string_in_sinusoidal_pattern.cpp

---
+++ print_string_in_sinusoidal_pattern.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main(){
+   string str="Hello_world!";
+   cout<<" ";
+   for(int i=1;i<str.length();i+=4){
+    cout<<str[i]<<"   ";
+   }
+   cout<<endl;
+   for (int i = 0; i < str.length(); i+=2)
+   {
+       cout << str[i] << " ";
+   }
+   cout<<endl<<"   ";
+   for (int i = 3; i < str.length(); i+=4)
+   {
+       cout << str[i] <<"   ";
+   }
+}
