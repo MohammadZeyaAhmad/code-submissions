ed201a425921ee6647c2c6c696623918c6f12534
Mohammad Zeya Ahmad <mohammadzeyaahmad@gmail.com>
2025-02-04 05:48:32 +0530

    improved spacing

--- print_string_in_sinusoidal_pattern.cpp
+++ print_string_in_sinusoidal_pattern.cpp
@@ -5,18 +5,18 @@
 using namespace std;
 int main(){
    string str="Hello_world!";
-   cout<<" ";
+   cout<<"  ";
    for(int i=1;i<str.length();i+=4){
-    cout<<str[i]<<"   ";
+    cout<<str[i]<<"     ";
    }
    cout<<endl;
    for (int i = 0; i < str.length(); i+=2)
    {
-       cout << str[i] << " ";
+       cout << str[i] << "  ";
    }
-   cout<<endl<<"   ";
+   cout<<endl<<"     ";
    for (int i = 3; i < str.length(); i+=4)
    {
-       cout << str[i] <<"   ";
+       cout << str[i] <<"     ";
    }
 }
